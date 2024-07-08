6c2c9ead3323bb13426be3929a5088f7e83a3e31
Christian Abarca <cabarca@wearegap.com>
2024-07-08 14:55:39 -0600

    add video true

---
+++ cypress/e2e/auto-practice1/auto-practice.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Student login', () => {
+    beforeEach(() => {
+        cy.visit('https://practicetestautomation.com/practice-test-login/')
+    })
+
+    it('can login with Secret Password - COPYTEST!', () => {
+        cy.get('#username').type('student')
+        cy.get('#password').type(`${Cypress.env('PASS_ONE')}`, { log: false })
+        cy.get('#submit').click()
+        cy.contains('Logged In Successfully')
+    })
+})
