3ea0a74805658c75766b7387bb7e6a9210fa41e5
Christian Abarca <cabarca@wearegap.com>
2024-07-07 20:02:48 -0600

    Add git ignore

--- cypress/e2e/utn/home.cy.js
+++ cypress/e2e/utn/home.cy.js
@@ -3,5 +3,9 @@ describe('Home', () => {
     it('Contenido Principal', () => {
         cy.visit('https://www.utn.ac.cr/admision')
         cy.contains("Proceso de Admisión")
+        cy.get("a[title='Expand menu Acerca de la UTN']").click()
+        cy.wait(5000)
+        cy.get("a[href*='marco-estrat']").first().click()
+        cy.contains("Contribuir a la equidad de oportunidades")
     })
 });
